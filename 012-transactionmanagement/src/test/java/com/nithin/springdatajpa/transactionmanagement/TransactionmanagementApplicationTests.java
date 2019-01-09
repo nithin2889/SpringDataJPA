241a5183070d7d2c7ff7ab9a44651e519661bf93
Nithin <nithin2889@gmail.com>
2019-01-09 20:11:04 +0530

    Spring Data JPA Using Hibernate.

--- 012-transactionmanagement/src/test/java/com/nithin/springdatajpa/transactionmanagement/TransactionmanagementApplicationTests.java
+++ 012-transactionmanagement/src/test/java/com/nithin/springdatajpa/transactionmanagement/TransactionmanagementApplicationTests.java
@@ -5,19 +5,32 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.nithin.springdatajpa.transactionmanagement.services.BankAccountService;
+import com.nithin.springdatajpa.transactionmanagement.entities.BankAccount;
+import com.nithin.springdatajpa.transactionmanagement.entities.repos.BankAccountRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TransactionmanagementApplicationTests {
 
 	@Autowired
-	BankAccountService service;
+	BankAccountRepository repository;
 	
 	@Test
-	public void testTransfer() {
-		service.transfer(500);
+	@Transactional
+	public void testTransfer(int amount) {
+		BankAccount obamasAccount = repository.findById(1).get();
+		obamasAccount.setBal(obamasAccount.getBal() - amount);
+		repository.save(obamasAccount);
+		
+		if(true) {
+			throw new RuntimeException();
+		}
+		
+		BankAccount trumpsAccount = repository.findById(2).get();
+		trumpsAccount.setBal(trumpsAccount.getBal() + amount);
+		repository.save(trumpsAccount);
 	}
-	
+
 }
